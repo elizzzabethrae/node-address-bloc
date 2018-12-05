799a68b137ca1ec0d8957ed2d5ab7d864eb97ab5
elizzzabethrae <elizabethwarners@gmail.com>
2018-12-04 20:53:06 -0500

    somehow lost my date! back now

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -9,6 +9,7 @@ module.exports = class MenuController {
         message: "Please choose from an option below: ",
         choices: [
           "Add new contact",
+          "Get date",
           "Exit"
         ]
       }
@@ -22,6 +23,9 @@ module.exports = class MenuController {
       switch(response.mainMenuChoice){
         case "Add new contact":
           this.addContact();
+          break;
+        case "Get date":
+          this.getDate();
           break;
         case "Exit":
           this.exit();
@@ -44,6 +48,22 @@ module.exports = class MenuController {
     console.log('addContact called');
     this.main();
   }
+
+  getDate(){
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+      if(dd<10) {
+        dd = '0'+dd
+      }
+      if(mm<10) {
+        mm = '0'+mm
+      }
+   today = mm + '/' + dd + '/' + yyyy;
+   console.log(today);
+  }
+
 
   exit(){
     console.log("Thanks for using AddressBloc!");
