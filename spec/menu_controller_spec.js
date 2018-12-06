489d3a70de32cbab4c9142a3a72bc03b64e7c6b8
elizzzabethrae <elizabethwarners@gmail.com>
2018-12-05 22:43:09 -0500

    Contacts and database checkpoint work

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -7,18 +7,4 @@ describe("MenuController", () => {
         this.menu = new MenuController();
       });
 
-    describe("#getContactCount()", () => {
- 
- // #2
-      it("should return 0 when no contacts are in the book", () => {
-        expect(this.menu.getContactCount()).toBe(0);
-      });
-
-      it("should return 1 when there is exactly one contact in the book", () => {
-        this.menu.contacts.push("Bob");
-        expect(this.menu.getContactCount()).toBe(1)
-      });
-
-    });
-    
   });
